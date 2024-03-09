92c7a7adf12c983c9dab738a82dc49028e150273
Hp <preethisaivungarala23@gmail.com>
2024-03-09 23:00:07 +0530

    assignment1

---
+++ main.py
@@ -0,0 +1,115 @@
+#convert kg into gms
+#kg= 56
+#gm= kg*1000
+#print("56 kg is equal to gm",gm)
+
+#Write a program to covert temperature from degree C to F. (Input 80C)
+#(80°C × 9/5) + 32 = 176°F
+#cel =80
+#farenheit = (cel * 9/5) +32
+#print(" 80c is equal to farenheit", farenheit)
+
+#Declare and initialize 3 three variable and print the biggest number
+#a=10;
+#b=20;
+#c=15;
+#big_number =max(a,b,c)
+#print("The biggest number from", a, b," and ", c, " is ", big_number)
+
+#Write a java program that performs the following tasks.
+#a. Store a number in a variable
+#a=10
+#print("the number stored is a variable", a)
+
+#b. If value is not in range (100-1000) prints wrong number else follows the steps
+#number=250;
+#if(100 <= number <= 1000):
+#    print("number is with in the range")
+#else:
+#   print("number is not in the range")
+
+#c. Check even or odd
+#a= 12
+#if (a % 2 == 0):
+ #   print("a is even")
+#else:
+ #print("a is odd")
+
+#d. If even divide the number by 3 and print the remainder
+#i = 14
+#if (i % 2 == 0):
+#    r=i % 3
+ #   print("i is even When it is divided by 3 the remainder is",r)
+#else:
+ #print("i is odd")
+
+#e. If odd divide the number by 2 and print the remainder
+#i=11;
+#if(i%2!=0):
+#    r=i%3
+ #   print("i is odd when it is divided by 3 and the reminder is",r)
+#else:
+ #print("i is the even number")
+
+ #5. Declare & initialize a number. Check whether the number is in range 0-100 or not. If not in range print invalid input. Else – if the number is in range 90-
+#100 then print Super Smart, 80-90 print Smart,70-80 print smart enough,60-70 print just smart, 35-60 print no smart, 0-35 print dump.
+#a=94;
+#if(a>=0 and a<=100):
+ #  if (a>=90 and a<=100):
+  #     print("super smart")
+   #    if (a>=80 and a<=90):
+    #       print("smart")
+     #  elif (a >= 70 and a <= 80):
+      #     print("smart enough")
+       #elif(a>=60 and a<=70):
+        #       print("just smart")
+       #elif(a>=35 and a<=60):
+          # print("no smart")
+   #elif(a>=0 and a<=35 ):
+    #       print("dump")
+
+#write a program to perform simple math based on user inputs by using switch condition(+,-,*/) scala
+
+#7. Write a program to print “SEEKHO BIGDATA”for 60 times.
+#for _ in range(60):
+ #print("SEEKHO BIGDATA")
+
+#8. Write a program to print all numbers which are divisible by 11 from 250 to 550
+#i = 250
+#while (i <= 550):
+ #   if (i % 11 == 0):
+  #      print(i)
+   # i += 1
+
+
+
+#10. Write a program to print all even numbers in range 700 to 900
+#a=700
+#b=900
+#for i in range(a,b):
+ #   if (i % 2 == 0):
+  #      print(i)
+
+#11. Write a program to print all odd numbers from 251 to 51. like (251,249,...51)
+#a=251;
+#b=51;
+#for i in range(a, b, -2):
+ #   print(i)
+
+#13. Write a program to print alternate even numbers from 20 to 140. Like (20,24,28...)
+
+#for i in range(20,140,4):
+ #   print(i)
+
+#14. Write a program to print alternate even numbers from 20 to 140. Like (22,26,30...)
+#for i in range(22,140,4):
+#    print(i)
+
+#16. Write a program to sum all even numbers between 382 and 582.
+#sum=0;
+#for i in range(382, 583):
+ #   if(i%2==0):
+  #      sum+=i
+   #     print("The sum of even numbers between 382 and 582 is", sum  )
+
+#
